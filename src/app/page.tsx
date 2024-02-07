fd9fd2533b5191d677b7c4e1ed697b236188368b
ryderhornedesign <ryderhornedev@gmail.com>
2024-02-06 19:03:39 -0700

    Fixed website link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <h1 className="text-5xl">Petite Curio Boutique</h1>
       </header>
       <nav className="flex flex-col flex-wrap justify-center items-center text-center space-y-2 w-full">
-        <LinkButton className="w-4/5" href="https://www.petitecurioboutique">Website</LinkButton>
+        <LinkButton className="w-4/5" href="https://www.petitecurioboutique.com">Website</LinkButton>
         <LinkButton className="w-4/5" href="https://www.instagram.com/petitecurioboutique">Instagram</LinkButton>
         <LinkButton className="w-4/5" href="https://www.facebook.com/PetiteCurioBoutique">Facebook</LinkButton>
         <LinkButton className="w-4/5" href="https://www.twitter.com/CurioPetite">Twitter</LinkButton>
